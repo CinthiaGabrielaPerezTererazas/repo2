dfa2fc70da2d5bdd8545ca17299fea966276420c
FlutLab.io <>
2022-05-03 19:51:16 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:perez/models/catalog.dart';
+import 'package:perez/widgets/itemWidget.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Adopciones"),
+        ),
+        body: ListView.builder(
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: CatalogModel.items[index]);
+            }),
+      ),
+    );
+  }
+}
